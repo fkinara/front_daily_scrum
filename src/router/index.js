e3c80ee1cf13017a80d949a01c775e06eb53b846
fkinara <42002518+fkinara@users.noreply.github.com>
2020-04-03 13:38:33 +0700

    login register

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,53 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Daily from '../views/Daily.vue'
+import Navbar from '../views/layouts/Navbar.vue'
+import Footer from '../views/layouts/Footer.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    components: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/register',
+    name: 'register',
+    components : Register,
+    // meta:{
+    //   requiresAuth: true
+    // }
+  },
+  {
+    path: '/daily',
+    name: 'daily',
+    components: {default:Daily , header: Navbar , footer: Footer},
+    meta:{
+      requiresAuth: true
+    }
+  },
+
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
 })
 
+// router.beforeEach((to, from, next) => {
+//   if(to.matched.some(record => record.meta.requiresAuth)) {
+//     if (store.getters.isLoggedIn) {
+//       next()
+//       return
+//     }
+//     next('/login') 
+//   } else {
+//     next() 
+//   }
+// })
 export default router
